34623d151ef2c91c6de2f897d7976f300ec2f11b
Lary Walburn II <127873103+Laryshane@users.noreply.github.com>
2023-03-16 16:17:00 -0500

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -1,25 +1,31 @@
 # Code used to program Smart Lights, as well as to see if lights are on or off.
 
 print("Please use Military Time Format (e.g. HH.MM, HH = 01-24, MM = 01-59)") # To explain Time Format.
-When_on = float(input("When Should Lights Turn On?: ")) # To get users desired time to turn lights on.
 
-When_off = float(input("When Should Lights Turn Off?: ")) # To get users desired time to turn lights off.
-if When_off < 00.01 or When_off > 24.00 or When_on < 00.01 or When_on > 24.00:
-  exit() # Added to avoid an invalid Time.
-else:
-  print("Light Schedule:", When_on, "TO", When_off) # To show/confirm users Time Schedule.
+When_on = float(input("When Should Lights Turn On?: ")) 
 
-print("Please type Yes or No") # To explain what input is needed for output.
-Question = input("Check if Lights are ON?: ") # To get users input.
+When_off = float(input("When Should Lights Turn Off?: ")) 
 
-if Question in ["No","no","NO", "nO", "n0", "N0"]: # Different ways the input No could be typed.
-  print("Thank you!") 
-else:
-  print("Please use Military Time Format (e.g. HH.MM, HH = 01-24, MM = 01-59)")
-  Wht_time = float(input("What Time is it?: "))# To get users current time.
-  if Wht_time < 00.01 or Wht_time > 24.00:
-    exit() # Added to avoid an invalid Time.
-  if Wht_time > When_on and Wht_time < When_off: # Users time needs to be greater than the scheduled start time but less than their scheduled end time.
-    print("Lights are ON!") # Tell user their Lights are ON.
+# LINE 10 is To make sure user can't enter an input less than 00.01 or higher than 24.00.
+# LINE 13 is used to have user enter only Yes or No exactly.
+# LINE 16 are all the ways "No" could be entered.
+# LINE 22 is the same reason as LINE 10.
+# LINE 23 is if User breaks time rule in LINE 20.
+# LINE 31 is if user breaks time ruin in LINE 15.
+if When_off == When_off > 00.01 and When_off <= 24.00 and When_on == When_on > 00.01 and When_on <= 24.00: 
+  print("Light Schedule:", When_on, "TO", When_off) 
+  print("Please type Yes or No:")
+  Question = input("Check if Lights are ON?: ")
+  if Question not in ["No", "nO", "no", "N0", "n0"]: 
+    print("Please use Military Time Format (e.g. HH.MM, HH = 01-24, MM = 01-59)")
+    Wht_time = float(input("What Time is it?: "))
+    if Wht_time > When_on and Wht_time < When_off:
+      print("Lights are ON!")
+    elif Wht_time < 00.01 or Wht_time > 24.00:
+      print("Try Again")
+    else:
+      print("Lights are OFF!")
   else:
-     print("Lights are OFF!") # Tell user their Lights are OFF.
+    print("Thank You!")
+else:
+  print("Try Again!")
